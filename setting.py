b007e5300cf0ff7ba3f8ae756b6702fa9fb48ee5
Nithin029 <82860635+Nithin029@users.noreply.github.com>
2024-04-02 17:02:11 +0530

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,5 +1,5 @@
-SEARCH_KEY= 'AIzaSyDHzZpiLttMYuM2X6EOUDWDRZWFWa3St5Y'
-SEARCH_ID= 'c374498e5007f4ef1'
+SEARCH_KEY= ''
+SEARCH_ID= ''
 
 
 SEARCH_URL = 'https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={q}&start={start}'
